384b306dce2e3d14c54ff59712ea1155651bfc15
bg-my99 <bg.m99@outlook.com>
2022-10-29 12:36:52 +0100

    Fixed framerate to cap at 60

--- src/__main__.py
+++ src/__main__.py
@@ -6,12 +6,13 @@ def main():
     pygame.init()
     
     screen = pygame.display.set_mode([500, 500])
+    clock = pygame.time.Clock()
     
     running = True
-    player1x = 250
+    player1x = 50
     player1y = 250
     
-    player2x = 250
+    player2x = 450
     player2y = 250
     
     while running:
@@ -29,8 +30,10 @@ def main():
         screen.fill((255, 255, 255))
     
         pygame.draw.circle(screen, (0, 0, 0), (player1x, player1y), 5)
+        pygame.draw.circle(screen, (0, 0, 0), (player2x, player2y), 5)
     
         pygame.display.flip()
+        clock.tick(60)
 
     pygame.quit()
 
